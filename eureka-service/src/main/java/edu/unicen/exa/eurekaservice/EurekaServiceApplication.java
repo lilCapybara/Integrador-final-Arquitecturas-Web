e961a29e7ed805ccd77d2be3d9c48431e5f2ef43
Sebastian Ortega <sortega@exa.unicen.edu.ar>
2024-11-06 17:17:19 -0300

    Agregados el gateway, confic-service y eureka-service

---
+++ eureka-service/src/main/java/edu/unicen/exa/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.unicen.exa.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
---
+++ gateway/src/test/java/edu/unicen/exa/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unicen.exa.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
