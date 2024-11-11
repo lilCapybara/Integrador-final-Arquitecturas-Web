64074f25b63640b8996aaeeed49f6ced301a8f49
Sebastian Ortega <sortega@alumnos.exa.unicen.edu.ar>
2024-11-11 12:18:46 -0300

    Agregado abm de la clase parada

--- microservicio-parada/src/main/java/Entities/Parada.java
+++ microservicio-parada/src/main/java/Entities/Parada.java
@@ -1,6 +1,8 @@
 package Entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -10,8 +12,11 @@ public class Parada {
     @Id
     private int idParada;
 
+    @Getter
+    @Setter
     private int posX;
 
+    @Getter @Setter
     private int posY;
 
 }
---
+++ microservicio-parada/src/main/java/Repositories/ParadaRepository.java
@@ -0,0 +1,7 @@
+package Repositories;
+
+import Entities.Parada;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParadaRepository extends JpaRepository<Parada, Integer> {
+}
